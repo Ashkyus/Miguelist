83209e7cdca66bde6a2869e64281ecdcae6e4360
Ashkyus <miguel_amg123@hotmail.com>
2026-02-13 15:37:01 +0100

    create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Header from './components/uiComponents/header'
 
 function App() {
 
   return (
     <>
+    <Header />
     </>
   )
 }
---
+++ src/components/uiComponents/header.tsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+
+    return(
+        <div className="w-full h-full bg-white">
+
+        </div>
+    )
+}
+
+export default Header;
